2086b109cc0209ee976f537731a43e40f12e7d47
Francisco J Bravo <fjbravo75@hotmail.com>
2026-03-16 10:07:39 +0100

    Add task creation flow for web UI

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from tasks.views import task_list
+from tasks.views import task_create, task_list
 
 urlpatterns = [
     path('', task_list, name='task_list'),
+    path('create/', task_create, name='task_create'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
+from tasks.forms import TaskForm
 from tasks.models import Task
 
 
 def task_list(request):
     tasks = Task.objects.all()
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+
+def task_create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+
+    return render(request, 'tasks/task_form.html', {'form': form})
