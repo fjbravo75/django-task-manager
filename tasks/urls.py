95ab44a60030514e3bc9ad39bbee62466d263bae
Francisco J Bravo <fjbravo75@hotmail.com>
2026-03-16 12:08:45 +0100

    Close task detail view with final visual fixes

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from tasks.views import task_create, task_list
+from tasks.views import task_create, task_detail, task_list
 
 urlpatterns = [
     path('', task_list, name='task_list'),
+    path('<int:pk>/', task_detail, name='task_detail'),
     path('create/', task_create, name='task_create'),
 ]
