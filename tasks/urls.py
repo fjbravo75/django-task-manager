cddb5b73209e2454d3c647b4bf9f446c14a841f3
Francisco J Bravo <fjbravo75@hotmail.com>
2026-03-17 08:00:17 +0100

    Complete visible task CRUD outside admin

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from tasks.views import task_create, task_detail, task_list
+from tasks.views import task_create, task_delete, task_detail, task_list, task_update
 
 urlpatterns = [
     path('', task_list, name='task_list'),
     path('<int:pk>/', task_detail, name='task_detail'),
+    path('<int:pk>/edit/', task_update, name='task_update'),
+    path('<int:pk>/delete/', task_delete, name='task_delete'),
     path('create/', task_create, name='task_create'),
 ]
