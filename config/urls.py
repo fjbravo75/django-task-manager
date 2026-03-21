b9ba7baafc05ba12d09fced7bba82e3a59b497f9
Francisco J Bravo <fjbravo75@hotmail.com>
2026-03-21 07:30:12 +0100

    Add basic authentication flows

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
+from tasks.views import register
 
 urlpatterns = [
+    path("accounts/register/", register, name="register"),
+    path(
+        "accounts/login/",
+        auth_views.LoginView.as_view(
+            template_name="registration/login.html",
+            redirect_authenticated_user=True,
+        ),
+        name="login",
+    ),
+    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
     path('', include('tasks.urls')),
     path('admin/', admin.site.urls),
 ]
